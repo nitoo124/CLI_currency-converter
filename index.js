d2728cab5372fe1e4ed13da514d8c2cba1d39194
Nitoo kumari <160069703+nitoo124@users.noreply.github.com>
2024-04-18 13:04:23 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.green.bold('\n \t Welcome Everyone! \n'));
+let coversion_rate = {
+    'USD': 1,
+    'CAD': 1.3,
+    'EUR': 0.9,
+    'PKR': 280,
+};
+let answers = await inquirer.prompt([
+    {
+        name: 'FROM',
+        type: 'list',
+        choices: ['USD', 'CAD', 'EUR', 'PKR'],
+        message: 'Select the currency to convert from?'
+    },
+    {
+        name: 'TO',
+        type: 'list',
+        choices: ['USD', 'CAD', 'EUR', 'PKR'],
+        message: 'Select the currency to convert from?'
+    },
+    {
+        name: 'amount',
+        type: 'number',
+        message: 'Entre your amount?'
+    },
+]);
+let from_amount = coversion_rate[answers.FROM];
+let to_amount = coversion_rate[answers.TO];
+let amount = answers.amount;
+//formula of currency conversion
+let base_amount = amount / from_amount;
+let converted_amount = base_amount * to_amount;
+console.log(chalk.blueBright.bold(`your converted amount is ${converted_amount.toFixed(2)}`));
